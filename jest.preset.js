eb20b097ac78dac3d39bb15c2b96f1da1ca1f8f3
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-10-23 22:18:49 -0300

    chore(jest): remove maxWorkers configuration from Jest settings

--- jest.preset.js
+++ jest.preset.js
@@ -2,7 +2,6 @@ const nxPreset = require('@nx/jest/preset').default;
 
 module.exports = {
 	...nxPreset,
-	maxWorkers: '50%',
 	cache: true,
 	cacheDirectory: '<rootDir>/node_modules/.cache/jest',
 	testTimeout: 10000,
